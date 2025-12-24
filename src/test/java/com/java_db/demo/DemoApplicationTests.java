8e716821d2652d199150058ed2363d33bcfdaaa7
imicola <imicola@outlook.com>
2025-12-24 09:19:39 +0800

    这是一次仓库的初始化 目标是进行哈尔滨理工大学课程设计 课程设计架构确定：
    车辆租贷管理系统
    
    后端
    Java21(openjdk)+spring boots 3.1 + PostgreSQL
    中间库
    Axios
    前端
    vite + React

---
+++ src/test/java/com/java_db/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java_db.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
